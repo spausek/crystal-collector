342674b67775f59c5477e573372c46608ed72f96
spausek <31174081+spausek@users.noreply.github.com>
2017-12-12 16:21:47 -0800

    Added images and basic HTML
    Basic HTML layout added. Random number fuction added in js for mystery number

---
+++ assets/game.js
@@ -0,0 +1,33 @@
+ $(document).ready(function() {
+
+//global variables
+
+var redRupee;
+var blueRupee;
+var greenRupee;
+var redRupee;
+var mysteryNumber;
+var score;
+var wins = 0;
+var losses = 0;
+
+//Start of game
+
+function gameStart() {
+
+	mysteryNumber = Math.floor(Math.random() *101) + 19;
+
+	console.log(mysteryNumber);
+
+
+
+
+}
+
+gameStart();
+
+
+
+
+
+ });
