8620b331031f6707291c4271f3ae0e5fa635942e
spausek <31174081+spausek@users.noreply.github.com>
2017-12-15 00:03:26 -0800

    Basic functions working
    Added on.click funtions for all rupee buttons. Basic fuinctions of the game appear to be working.

--- assets/game.js
+++ assets/game.js
@@ -2,7 +2,10 @@
 
 //global variables
 
-
+var redRupee;
+var blueRupee;
+var greenRupee;
+var redRupee;
 var mysteryNumber;
 var score;
 var wins = 0;
@@ -11,22 +14,102 @@ var losses = 0;
 //Start of game
 
 function gameStart() {
-
+	score = 0;
 	mysteryNumber = Math.floor(Math.random() *101) + 19;
+	yellowRupee = Math.floor(Math.random()* 12) +1;
+	redRupee = Math.floor(Math.random()* 12) +1;
+	greenRupee = Math.floor(Math.random()* 12) +1;
+	blueRupee = Math.floor(Math.random()* 12) +1;
+	$('#mystery-number').html(mysteryNumber)
+	$('#user-score').html(score);
+
+
 
 	console.log(mysteryNumber);
-	var numberArray =  [];
-
-	for (var i = 0; i < 4; i ++) {
-		var randomNumber = Math.floor(Math.random() *12) +1;
-		console.log(randomNumber);
-	}
-
-
-
+	console.log(yellowRupee);
+	console.log(redRupee);
+	console.log(greenRupee);
+	console.log(blueRupee);
+	
 
 
 }
+
+function youWin () {
+
+	alert("You have solved they mystery!")
+	wins ++;
+	$("#win").html(wins);
+	gameStart();
+
+}
+
+function youLose (){
+
+	alert("The Crystals have retained their secrets! Hehehehehe")
+	losses ++;
+	$('#loss').html(losses);
+	gameStart();
+}
+
+
+
+$('#yellow').on('click', function(){
+	score = score + yellowRupee;
+	$('#user-score').html(score);
+
+	if (score === mysteryNumber) {
+		youWin ();
+	}
+
+	else if (score > mysteryNumber) {
+		youLose();
+	}
+
+});
+
+$('#red').on('click', function(){
+	score = score + yellowRupee;
+	$('#user-score').html(score);
+
+	if (score === mysteryNumber) {
+		youWin ();
+	}
+
+	else if (score > mysteryNumber) {
+		youLose();
+	}
+
+});
+
+$('#blue').on('click', function(){
+	score = score + yellowRupee;
+	$('#user-score').html(score);
+
+	if (score === mysteryNumber) {
+		youWin ();
+	}
+
+	else if (score > mysteryNumber) {
+		youLose();
+	}
+
+});
+
+$('#green').on('click', function(){
+	score = score + yellowRupee;
+	$('#user-score').html(score);
+
+	if (score === mysteryNumber) {
+		youWin ();
+	}
+
+	else if (score > mysteryNumber) {
+		youLose();
+	}
+
+});
+
 
 gameStart();
 
